ec6acbc20cd90a0e01ef0c9fc582dcc5df7b77b6
TristansCloud <tristankosciuch@gmail.com>
2020-04-07 21:24:08 -0400

    checking master LTT

--- rscript/gams.R
+++ rscript/gams.R
@@ -1,48 +1,42 @@
-capwords <- function(s, strict = FALSE) {
-  cap <- function(s) paste(toupper(substring(s, 1, 1)),
-                           {s <- substring(s, 2); if(strict) tolower(s) else s},
-                           sep = "", collapse = " " )
-  sapply(strsplit(s, split = " "), cap, USE.NAMES = !is.null(names(s)))
-}
+library(tidyverse)
+library(lubridate)
 
-ltt<-read_csv("ltt-with-NAs.csv") %>% 
-  mutate(net = toupper(net),
-         comments = toupper(comments),
-         site = capwords(site),
-         cpue_id = paste(year,month,day,site_type,net,sep = "-"),
-         id = paste(year,month,day,tag),
-         dist_shore = case_when(net == "5" ~ "inshore",
-                                net == "20" ~ "inshore",
-                                net == "100" ~ "offshore",
-                                net == "20 EXTRA" ~ "inshore",
-                                net == "100 EXTRA" ~ "offshore",
-                                net == "5 EXTRA" ~ "inshore",
-                                net == "MID" ~ "offshore",
-                                net == "21 EXTRA" ~ "inshore",
-                                net == "22 EXTRA" ~ "inshore",
-                                net == "101 EXTRA" ~ "offshore",
-                                TRUE ~ net)) %>% 
-  filter(!grepl('MUK', comments),
-         !grepl('MUK', net),
-         !grepl('BOUGHT', net),
-         !grepl('BOUGHT', comments))
+#######################
+## Errors to fix in LTT:
+##    fish_code            tl              sl 
+##    Max.   :53.000   Max.   :910.00   Max.   :240.000 
+##     - tl and sl are wildly different max values
+##     - fish_code doesn't go to 53
 
-# Get number of samples for group
+## ltt-with-NAs.csv had 35048 rows after filtering
+## Shelby's master had 39119 rows after filtering
 
-
-intermediate<-ltt %>% 
-  filter(fish_code==7) %>% 
-  group_by(year,month) %>% 
-  count(year)
-intermediate %>%
-  group_by(year) %>% 
-  count(year)
-
+LTT<-read_csv("ltt-master.csv") %>% 
+  mutate(Net = toupper(Net),
+         Comments = toupper(Comments),
+         Site = toupper(Site),
+         cpue_id = paste(`Date (DD-Mon-YY)`,Net,sep = "-"), # probably change colname for publication
+         np_id = paste(`Date (DD-Mon-YY)`,Tag_Num),
+         dist_shore = case_when(Net == "5" ~ "inshore",
+                                Net == "20" ~ "inshore",
+                                Net == "100" ~ "offshore",
+                                Net == "20 EXTRA" ~ "inshore",
+                                Net == "100 EXTRA" ~ "offshore",
+                                Net == "5 EXTRA" ~ "inshore",
+                                Net == "MID" ~ "offshore",
+                                Net == "21 EXTRA" ~ "inshore",
+                                Net == "22 EXTRA" ~ "inshore",
+                                Net == "101 EXTRA" ~ "offshore",
+                                TRUE ~ Net)) %>% 
+  filter(!grepl('MUK', Comments),
+         !grepl('MUK', Net),
+         !grepl('BOUGHT', Net),
+         !grepl('BOUGHT', Comments))
 
 
 ## Group by distance from shore:
 ##  - NP SL
-##  - NP abundance
+##  - NP CPUE ensure 0 catch days included
 ##  - NP mass and mass of daily catch
 ##  - Hap CPUE
 ##
@@ -55,3 +49,17 @@ intermediate %>%
 ##  hap cpue and stomach gams to test goodness of fit (MSE, R^2).
 ##  
 
+
+## NP SL
+
+NP<-LTT %>% 
+  filter(Fish_Code==7,!is.na(SL))  #fix these missing SL values! Also missing tags
+
+# number of NP per month
+inter<-NP %>% 
+  group_by(Year,Month) %>% 
+  count(Tag_Num)
+counts<-inter %>% 
+  group_by(Year,Month) %>% 
+  sum(n)
+rm(inter);view(counts)
